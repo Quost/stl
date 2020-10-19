2d0ee0d5387dd4e4e12a97feb652ec46e6e223e5
Matheus Quost <matheusquost@gmail.com>
2020-10-19 13:33:01 -0300

    labels acionadas ao pressionar objeto selecionável

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 import * as THREE from "https://threejs.org/build/three.module.js";
 import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
 
-var tipoTextoAnterior = new Array(1, 1, 1),
-    tipoTexto = new Array(1, 1, 1),
+let tipoTextoAnterior = new Array(),
+    tipoTexto = new Array(),
     pickHelper,
     buttons,
     camera,
@@ -17,12 +17,13 @@ const canvas = document.querySelector('#c'),
     arrayElements = new Array(),
     tempV = new THREE.Vector3(),
     mouse = new THREE.Vector2(),
-    projector = { x: 0, y: 0 },
     mensagens = new Array(),
     elements = new Array(),
     helper = new Array(),
+    info2 = new Array(),
     info = new Array(),
     idToObject = {},
+    old = false,
     ids = {};
 
 carregaBotoes();
@@ -30,11 +31,8 @@ pegarDadosXML();
 
 function init(infoElementos) {
 
-    console.log(infoElementos);
-
     const titulos = (infoElementos.getElementsByTagName("titulo"));
     numObjects = titulos.length;
-    console.log(numObjects);
     const posx = (infoElementos.getElementsByTagName("posx"));
     const posy = (infoElementos.getElementsByTagName("posy"));
     const posz = (infoElementos.getElementsByTagName("posz"));
@@ -46,7 +44,6 @@ function init(infoElementos) {
             this.id = id;
             this.marker = "";
             this.titulo = titulos[id].firstChild.nodeValue;
-            console.log(this.titulo)
             this.posx = posx[id].firstChild.nodeValue;
             this.posy = posy[id].firstChild.nodeValue;
             this.posz = posz[id].firstChild.nodeValue;
@@ -73,13 +70,11 @@ function init(infoElementos) {
     let controls = new OrbitControls(camera, canvas);
 
     let box = new THREE.Mesh(new THREE.BoxBufferGeometry(10, 10, 10), new THREE.MeshNormalMaterial());
-    console.log("id box: " + box.id);
     ids[0] = box.id;
     scene.add(box);
 
     let box2 = new THREE.Mesh(new THREE.BoxBufferGeometry(50, 5, 5), new THREE.MeshNormalMaterial());
     ids[1] = box2.id;
-    console.log("id box2: " + box2.id);
     scene.add(box2);
     box2.position.set(0, -2.5, 0);
 
@@ -90,20 +85,28 @@ function init(infoElementos) {
         arrayElements[i].marker = new THREE.Mesh(new THREE.CylinderBufferGeometry(0.25, 0.25, 0.5, 16), new THREE.MeshBasicMaterial({ color: "#FFFF00" }));
         arrayElements[i].marker.geometry.translate(0, 0.25, 0);
         arrayElements[i].marker.position.set(arrayElements[i].posx, arrayElements[i].posy, arrayElements[i].posz);
-        arrayElements[i].marker.name = i;
+
         scene.add(arrayElements[i].marker);
-        idToObject[i] = arrayElements[i].marker;
-        mensagens[i] = arrayElements[i].titulo + "\nTemperatura: " + arrayElements[i].temp + "\nFuncionamento: " + arrayElements[i].func + ".";
-        const elem = document.createElement('div');
-        elem.textContent = mensagens[i];
-        arrayElements[i].elem = elem;
-        labelContainerElem.appendChild(elem);
-        let auxMarker = arrayElements[i].marker;
-        elements.push({ auxMarker, elem });
-        helper[i] = 0;
+
+        let id = arrayElements[i].marker.id;
+        idToObject[id] = arrayElements[i];
+        mensagens[id] = arrayElements[i].titulo + "\nTemperatura: " + arrayElements[i].temp + "\nFuncionamento: " + arrayElements[i].func + ".";
+        tipoTexto[id] = 1;
+        tipoTextoAnterior[id] = 0;
+
+        // const elem = document.createElement('div');
+
+        // elem.textContent = mensagens[i];
+
+        // labelContainerElem.appendChild(elem);
+
+        // elements.push({ auxMarker, elem });
+        helper[id] = 0;
 
         pickingMarker[i] = new THREE.Mesh(new THREE.CylinderBufferGeometry(0.25, 0.25, 0.5, 16), new THREE.MeshBasicMaterial({ color: "#000022" }));
+
         pickingScene.add(pickingMarker[i]);
+
         pickingMarker[i].position.copy(arrayElements[i].marker.position);
         pickingMarker[i].rotation.copy(arrayElements[i].marker.rotation);
         pickingMarker[i].scale.copy(arrayElements[i].marker.scale);
@@ -131,16 +134,12 @@ function onDocumentMouseDown(event) {
     // (such as the mouse's TrackballControls)
     // event.preventDefault();
 
-    console.log("Click.");
-
     mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
     mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
 
     raycaster.setFromCamera(mouse, camera);
 
     var intersects = raycaster.intersectObjects(scene.children);
-
-    console.log("intersects: " + intersects);
 
     if (intersects.length > 0) {
         if (intersects[0].object.id != ids[0] && intersects[0].object.id != ids[1]) {
@@ -150,129 +149,69 @@ function onDocumentMouseDown(event) {
 }
 
 function displayInfo(pickedObjectId) {
-    console.log("displayInfo()");
-    console.log("pickedObjectId: " + pickedObjectId);
-
-    for (let i = 0; i < numObjects; i++) {
-        console.log("numObj: " + numObjects);
-        console.log("i: " + i);
-        console.log("arrayElements[i].marker.id: " + arrayElements[i].marker.id);
-        if (pickedObjectId == arrayElements[i].marker.id) {
-            if (tipoTexto[i] == 1) {
-                alert("tipoTexto: 1");
-                if (tipoTextoAnterior[i] == 2) {
-                    arrayElements[i].elem.style.display = 'none';
-                    helper[i] = 0;
-                    tipoTextoAnterior[i] = 1;
-                }
-                if (helper[i] == 0) {
-                    alert("helper: 0");
-                    var loader = new THREE.FontLoader();
-                    loader.load('https://threejs.org/examples/fonts/gentilis_regular.typeface.json', function(font) {
-                        var geometry = new THREE.TextGeometry(mensagens[i], {
-                            font: font,
-                            size: 1,
-                            height: 0.2,
-                            curveSegments: 4,
-                            bevelEnabled: true,
-                            bevelThickness: 0.02,
-                            bevelSize: 0.05,
-                            bevelSegments: 3
-                        });
-                        geometry.center();
-                        var material = new THREE.MeshNormalMaterial();
-                        var mesh = new THREE.Mesh(geometry, material);
-                        mesh.position.y = 7 + Number(arrayElements[i].marker.position.y);
-                        mesh.position.x = Number(arrayElements[i].marker.position.x);
-                        scene.add(mesh);
-                        info[i] = mesh;
-                        helper[i] = 1;
-                        renderer.render(scene, camera);
-                    });
-                } else if (helper[i] == 1) {
-                    alert("helper: 1");
-                    info[i].visible = false;
-                    helper[i] = 2;
-                } else {
-                    alert("helper: 2");
-                    info[i].visible = true;
-                    helper[i] = 1;
-                }
-            } else {
-                elements.forEach((elementInfo) => {
-                    alert("aqui");
-                    // if (auxMarker.id == pickedObjectId) {
-                    alert("here");
-                    const { auxMarker, elem } = elementInfo;
-
-                    // get the position of the center of the cube
-                    auxMarker.updateWorldMatrix(true, false);
-                    auxMarker.getWorldPosition(tempV);
-
-                    // get the normalized screen coordinate of that position
-                    // x and y will be in the -1 to +1 range with x = -1 being
-                    // on the left and y = -1 being on the bottom
-                    tempV.project(camera);
-
-                    // ask the raycaster for all the objects that intersect
-                    // from the eye toward this object's position
-                    raycaster.setFromCamera(tempV, camera);
-                    const intersectedObjects = raycaster.intersectObjects(scene.children);
-                    // We're visible if the first intersection is this object.
-                    const show = intersectedObjects.length && auxMarker === intersectedObjects[0].object;
-
-                    if (!show || Math.abs(tempV.z) > 1) {
-                        // hide the label
-                        arrayElements[i].elem.style.display = 'none';
-                    } else {
-                        mostrarLabel();
-                    }
-                    // }
-                });
-                alert("tipoTexto: 2");
-                if (tipoTextoAnterior[i] == 1) {
-                    info[i].visible = false;
-                    helper[i] = 0;
-                    tipoTextoAnterior[i] = 2;
-                }
-                if (helper[i] == 0) {
-                    alert("helper: 0");
-                    if (!arrayElements[i].show || Math.abs(tempV.z) > 1) {
-                        // hide the label
-                        arrayElements[i].elem.style.display = 'none';
-                    } else {
-                        // unhide the label
-                        mostrarLabel(i);
-                    }
-                    helper[i] = 1;
-                } else if (helper[i] == 1) {
-                    alert("helper: 1");
-                    elemento.elem.style.display = 'none';
-                    helper[i] = 2;
-                } else {
-                    alert("helper: 2");
-                    elemento.elem.style.display = '';
-                    helper[i] = 1;
-                }
+    if (pickedObjectId == idToObject[pickedObjectId].marker.id) {
+        if (tipoTexto[pickedObjectId] == 1) {
+            if (tipoTextoAnterior[pickedObjectId] == 2) {
+                info2[pickedObjectId].visible = false;
+                helper[pickedObjectId] = 0;
+                tipoTextoAnterior[pickedObjectId] = 1;
             }
-
+            tipoTextoAnterior[pickedObjectId] = 1;
+            if (helper[pickedObjectId] == 0) {
+                var loader = new THREE.FontLoader();
+                loader.load('https://threejs.org/examples/fonts/gentilis_regular.typeface.json', function(font) {
+                    var geometry = new THREE.TextGeometry(mensagens[pickedObjectId], {
+                        font: font,
+                        size: 1,
+                        height: 0.2,
+                        curveSegments: 4,
+                        bevelEnabled: true,
+                        bevelThickness: 0.02,
+                        bevelSize: 0.05,
+                        bevelSegments: 3
+                    });
+                    geometry.center();
+                    var material = new THREE.MeshNormalMaterial();
+                    var mesh = new THREE.Mesh(geometry, material);
+                    mesh.position.y = 7 + Number(idToObject[pickedObjectId].marker.position.y);
+                    mesh.position.x = Number(idToObject[pickedObjectId].marker.position.x);
+                    scene.add(mesh);
+                    info[pickedObjectId] = mesh;
+                    helper[pickedObjectId] = 1;
+                    renderer.render(scene, camera);
+                });
+            } else if (helper[pickedObjectId] == 1) {
+                info[pickedObjectId].visible = false;
+                helper[pickedObjectId] = 2;
+            } else {
+                info[pickedObjectId].visible = true;
+                helper[pickedObjectId] = 1;
+            }
+        } else {
+            if (tipoTextoAnterior[pickedObjectId] == 1) {
+                info[pickedObjectId].visible = false;
+                helper[pickedObjectId] = 0;
+                tipoTextoAnterior[pickedObjectId] = 2;
+            }
+            tipoTextoAnterior[pickedObjectId] = 2;
+            if (helper[pickedObjectId] == 0) {
+                var spritey = makeTextSprite(pickedObjectId);
+                spritey.position.y = 7 + Number(idToObject[pickedObjectId].marker.position.y);
+                spritey.position.x = Number(idToObject[pickedObjectId].marker.position.x);
+                scene.add(spritey);
+                info2[pickedObjectId] = spritey;
+                helper[pickedObjectId] = 1;
+                renderer.render(scene, camera);
+            } else if (helper[pickedObjectId] == 1) {
+                info2[pickedObjectId].visible = false;
+                helper[pickedObjectId] = 2;
+            } else {
+                info2[pickedObjectId].visible = true;
+                helper[pickedObjectId] = 1;
+            }
         }
+
     }
-}
-
-
-function mostrarLabel(i) {
-    arrayElements[i].style.display = '';
-
-    // convert the normalized position to CSS coordinates
-    const x = (tempV.x * .5 + .5) * canvas.clientWidth;
-    const y = (tempV.y * -.5 + .5) * canvas.clientHeight;
-
-    // move the elem to that position
-    arrayElements[i].style.transform = `translate(-50%, -50%) translate(${x}px,${y}px)`;
-
-    // set the zIndex for sorting
-    arrayElements[i].style.zIndex = (-tempV.z * .5 + .5) * 100000 | 0;
 }
 
 function pegarDadosXML() {
@@ -296,48 +235,150 @@ function carregaBotoes() {
 }
 
 function onButtonClick(event) {
-    if (tipoTexto == 1) {
-        tipoTexto = 2;
-        tipoTextoAnterior = 1;
+    let alerta;
+    for (let i = 0; i < numObjects; i++) {
+        let id = arrayElements[i].marker.id;
+
+        if (tipoTexto[id] == 1) {
+            tipoTexto[id] = 2;
+            alerta = 0;
+        } else {
+            tipoTexto[id] = 1;
+            alerta = 1;
+        }
+    }
+    if (alerta == 0) {
         alert("5a. Labels aparecem orientados de frente ao Canvas, independentemente da posição zoom ou orbit que se impõem ao canvas.\n\nClique em um objeto selecionável para atualizar!");
     } else {
-        tipoTexto = 1;
-        tipoTextoAnterior = 2;
         alert("5b. Labels sensíveis a posição de visão, orbit , zoom e pan, podendo ser até visto de trás\n\nClique em um objeto selecionável para atualizar!");
     }
 }
 
-function buscaElementoPorId(id) {
-    let aux;
-    class auxiliar {
-        constructor() {
-            this.elem = "";
-            this.show = "";
-        }
+function makeTextSprite(id, opts) {
+    var parameters = opts || {};
+    var fontface = parameters.fontface || 'Helvetica';
+    var fontsize = parameters.fontsize || 25;
+    var canvas = document.createElement('canvas');
+    canvas.id = "sprite";
+    var context = canvas.getContext('2d');
+    context.font = fontsize + "px " + fontface;
+
+    // get size data (height depends only on font size)
+    // var metrics = context.measureText(message);
+    // var textWidth = metrics.width;
+
+    // text color
+    context.fillStyle = 'rgba(255, 255, 255, 1.0)';
+    context.fillText(idToObject[id].titulo, 10, 25);
+    context.fillText("Temperatura: " + idToObject[id].temp, 10, 50);
+    context.fillText("Funcionamento: " + idToObject[id].func, 10, 75);
+
+    // canvas contents will be used for a texture
+    var texture = new THREE.Texture(canvas)
+    texture.minFilter = THREE.LinearFilter;
+    texture.needsUpdate = true;
+
+    var spriteMaterial = new THREE.SpriteMaterial({ map: texture });
+    var sprite = new THREE.Sprite(spriteMaterial);
+    sprite.scale.set(10, 5, 1.0);
+    // sprite.center.set(0, 1);
+
+    // get the position of the center of the cube
+    sprite.updateWorldMatrix(true, false);
+    sprite.getWorldPosition(tempV);
+
+    // get the normalized screen coordinate of that position
+    // x and y will be in the -1 to +1 range with x = -1 being
+    // on the left and y = -1 being on the bottom
+    tempV.project(camera);
+
+    // ask the raycaster for all the objects that intersect
+    // from the eye toward this object's position
+    raycaster.setFromCamera(tempV, camera);
+    const intersectedObjects = raycaster.intersectObjects(scene.children);
+    // We're visible if the first intersection is this object.
+    const show = intersectedObjects.length && sprite === intersectedObjects[0].object;
+
+    if (!show || Math.abs(tempV.z) > 1) {
+        // hide the label
+        canvas.style.display = 'none';
+    } else {
+        // unhide the label
+        canvas.style.display = '';
+
+        // convert the normalized position to CSS coordinates
+        const x = (tempV.x * .5 + .5) * canvas.clientWidth;
+        const y = (tempV.y * -.5 + .5) * canvas.clientHeight;
+
+        // move the elem to that position
+        canvas.style.transform = `translate(-50%, -50%) translate(${x}px,${y}px)`;
+
+        // set the zIndex for sorting
+        canvas.style.zIndex = (-tempV.z * .5 + .5) * 100000 | 0;
     }
-    elements.forEach((elementInfo) => {
 
-        const { auxMarker, elem } = elementInfo;
-        // get the position of the center of the cube
-        auxMarker.updateWorldMatrix(true, false);
-        auxMarker.getWorldPosition(tempV);
+    return sprite;
+}
 
-        // get the normalized screen coordinate of that position
-        // x and y will be in the -1 to +1 range with x = -1 being
-        // on the left and y = -1 being on the bottom
-        tempV.project(camera);
+function adaptCanvasToText(canvas, message, font_size, font_face) {
 
-        // ask the raycaster for all the objects that intersect
-        // from the eye toward this object's position
-        raycaster.setFromCamera(tempV, camera);
-        const intersectedObjects = raycaster.intersectObjects(scene.children);
-        // We're visible if the first intersection is this object.
-        const show = intersectedObjects.length && auxMarker === intersectedObjects[0].object;
-        if (elem.id == id) {
-            aux = new auxiliar();
-            aux.elem = elem;
-            aux.show = show;
+    "use strict";
+
+    var context = canvas.getContext('2d');
+
+    if (canvas.height > canvas.width) {
+        canvas.width = canvas.height;
+    }
+
+
+    while (true) {
+        var side = getNextPowerOfTwo(canvas.width);
+
+        if (side < 128) {
+            side = 128;
         }
-    });
-    return aux;
+
+        canvas.width = canvas.height = side;
+
+        context.font = "Bold " + font_size + "pt " + font_face;
+
+        var metrics = context.measureText(message);
+        var text_width = metrics.width;
+        var text_side = getNextPowerOfTwo(Math.max(text_width, font_size));
+
+        if (text_side >= 128) {
+            if (side !== text_side) {
+                canvas.width = text_side;
+                continue;
+            }
+        } else if (side !== 128) {
+            canvas.width = 128;
+            continue;
+        }
+
+        break;
+    }
+}
+
+function getNextPowerOfTwo(num) {
+
+    "use strict";
+
+    if (num < 0) {
+        throw new Error("Argument must be positive");
+    }
+
+    var bin_str = toBinaryInt(num - 1);
+
+    if (bin_str.indexOf("0") < 0 || bin_str === "0") {
+        return num;
+    } else {
+        return Math.pow(2, bin_str.length);
+    }
+}
+
+function toBinaryInt(num) {
+    "use strict";
+
+    return (num >>> 0).toString(2); // jshint ignore:line
 }
